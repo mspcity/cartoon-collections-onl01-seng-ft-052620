4f9d7c4089d2fb3f86d5d271e35db572e49a25de
Youngmin Park <codepark2019@gmail.com>
2020-05-30 23:53:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,13 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_planeteer_calls)
+  long_planeteer_calls.each do |call|
+   if call.length > 4
+     return true
+   end
+ end
+ false
 end
 
 def find_the_cheese# code an argument here
