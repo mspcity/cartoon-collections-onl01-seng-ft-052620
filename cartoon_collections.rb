7ab3618af39512aabf3fb832ec2c9c42d1649af4
Youngmin Park <codepark2019@gmail.com>
2020-05-30 23:32:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do | name, index |
+    puts "#{index + 1} #{name}"
+  
+
 end
 
 def summon_captain_planet# code an argument here
